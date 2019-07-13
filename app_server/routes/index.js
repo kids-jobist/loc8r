a0758b1c64f6ca3cabba28bbe43020e9701c6d6f
kids-jobist <kids.jobist@gmail.com>
2019-07-13 21:12:56 +0400

    Editing layout.jade

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require("../controllers/locations.js");
+var ctrlOhters = require("../controllers/others.js");
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Страницы местоположений */
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Другие страницы */ 
+router.get('/about', ctrlOhters.about);
 
 module.exports = router;
